c6fd8639400b1f4d375e5918d180255ace5ec1f0
Alphawizz Flutter Team <alphawizzflutterteam@gmail.com>
2024-08-16 15:48:29 +0530

    first commit

---
+++ lib/Helper/Constant.dart
@@ -0,0 +1,24 @@
+final String appName = 'KIRANU';
+
+final String packageName = 'com.kiranu.user';
+final String androidLink = 'https://play.google.com/store/apps/details?id=';
+
+final String iosPackage = 'com.kiranu.user';
+final String iosLink = 'your ios link here';
+final String appStoreId = 'com.kiranu.user';
+
+final String deepLinkUrlPrefix = 'https://alpha.ecommerce.link';
+final String deepLinkName = 'alpha.ecommerce.link';
+
+final int timeOut = 50;
+const int perPage = 10;
+
+// final String baseUrl = 'https://developmentalphawizz.com/kiranu/app/v1/api/';
+// final String baseUrl = 'https://kiranu.com/app/v1/api/';
+final String baseUrl = 'https://kiranu.com/app/v1/api/';
+
+
+
+final String imageUrl = 'https://developmentalphawizz.com/kiranu/';
+
+final String jwtKey = "e1e421dceee9ea718ab764ea4fdef95629e7f415";
